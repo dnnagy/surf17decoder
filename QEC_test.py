87f018232dd4ec8e8539ebe4132db691511099ca
Nagy Dániel <nagy@chromeshift.com>
2019-01-29 22:17:39 +0100

    gjgjgkl

--- QEC_test.py
+++ QEC_test.py
@@ -5,39 +5,31 @@ import pandas as pd
 import numpy as np
 import time
 
-cycle_lengths = [100, 150, 200]
 
-train_files = ['small_c{0}_train.db'.format(k) for k in cycle_lengths]
-val_files = ['small_c{0}_validation.db'.format(k) for k in cycle_lengths]
-test_files = ['small_c{0}_test.db'.format(k) for k in cycle_lengths]
-
-db_path = './data/' # /content/gdrive/My Drive/deeplea2f18em/qecdata/
-
-baseline=False
-
-print_t("train_files={}".format(train_files))
-
-for k in range(len(cycle_lengths)):
+def make_test(cycles, train_file, val_file, test_file):
     print_t("============================================================================")
-    print_t("Fitting model with cycle length {0}".format(cycle_lengths[k]))    
+    print_t("Fitting model with cycle length {0}".format(cycles))    
     print_t("============================================================================")
-    model, history, (bgt, bgv) = fit_model(db_path+train_files[k], 
-                               db_path+val_files[k],
-                               db_path+test_files[k], 
-                               batch_size=20, 
+
+    fit_baseline=False
+
+    model, history, (bgt, bgv) = fit_model(train_file, 
+                               val_file,
+                               test_file, 
+                               batch_size=64, 
                                early_stop=True, 
                                early_stop_min_delta=1e-7, 
-                               cycle_length=cycle_lengths[k],
+                               cycle_length=cycles,
                                n_epochs=50,
                                n_workers=96,
-                               baseline=baseline)
+                               baseline=fit_baseline)
 
-    datafile_prefix = 'baseline' if baseline==True else 'simpledec'
+    datafile_prefix = 'baseline' if fit_baseline==True else 'simpledec'
+    
     # Save history to a file
     dfhist = pd.DataFrame(history.history)
-    dfhist.to_csv(datafile_prefix + train_files[k] + "_history_" + time.strftime("%Y-%m-%d-%H-%M-%S") + ".csv")
+    dfhist.to_csv(datafile_prefix + train_file + "_history_" + time.strftime("%Y-%m-%d-%H-%M-%S") + ".csv")
 
-    
     # Calculate roc_auc and save fpr, tpr as a DataFrame
     Xs = [bgv.__getitem__(k)[0] for k in range(min(len(bgv), int(np.ceil(4000/bgv.batch_size))))]
     ys = [bgv.__getitem__(k)[1] for k in range(min(len(bgv), int(np.ceil(4000/bgv.batch_size))))]
@@ -49,8 +41,18 @@ for k in range(len(cycle_lengths)):
     fpr, tpr, thr = roc_curve(ys[:,0], y_pred[:,0])
     
     dfroc = pd.DataFrame({'fpr':np.array(fpr), 'tpr': np.array(tpr)})
-    dfroc.to_csv(datafile_prefix + train_files[k] + "_roc_" + time.strftime("%Y-%m-%d-%H-%M-%S") + ".csv")
+    dfroc.to_csv(datafile_prefix + train_file + "_roc_" + time.strftime("%Y-%m-%d-%H-%M-%S") + ".csv")
     
     print_t("AUC score={0}".format(auc(fpr, tpr)))
-    print_t("{0} done.".format(train_files[k]))
+    print_t("{0} done.".format(train_file))
     print_t("============================================================================")
+    
+    return
+
+
+db_path = './data/' # /content/gdrive/My Drive/deeplea2f18em/qecdata/
+cycles = 150
+make_test(cycles, 
+          db_path + 'small_c{0}_train.db'.format(cycles),
+          db_path + 'small_c{0}_validation.db'.format(cycles),
+          db_path + 'small_c{0}_test.db'.format(cycles))
